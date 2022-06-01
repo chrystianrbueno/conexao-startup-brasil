e2898f876b3c4afc4c3ca7a49415aa7f3c51e048
Marco Dalalba <vinisdl@hotmail.com>
2022-06-01 18:21:59 -0300

    adding investor and projet

--- src/main/java/br/unisinos/startup/connection/mapper/InvestorResponseMapper.java
+++ src/main/java/br/unisinos/startup/connection/mapper/InvestorResponseMapper.java
@@ -1,4 +1,22 @@
 package br.unisinos.startup.connection.mapper;
 
+import br.unisinos.startup.connection.controller.response.EntrepreneurResponseModel;
+import br.unisinos.startup.connection.controller.response.InvestorResponseModel;
+import br.unisinos.startup.connection.model.EntrepreneurModel;
+import br.unisinos.startup.connection.model.InvestorModel;
+
+import java.util.Optional;
+
 public class InvestorResponseMapper {
+    public static InvestorResponseModel mapFrom(InvestorModel investorResponseModel) {
+        return Optional.ofNullable(investorResponseModel).map(investor ->
+                InvestorResponseModel.builder()
+                        .id(investor.getId())
+                        .name(investor.getName())
+                        .age(investor.getAge())
+                        .city(investor.getCity())
+                        .mail(investor.getMail())
+                        .build()
+        ).orElse(null);
+    }
 }
