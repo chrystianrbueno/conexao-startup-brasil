2483b6eb6b3cdf9fa6cc0ac066088de2ceccc4d2
cbueno <cgbueno@grupodimed.com.br>
2022-06-14 22:33:10 -0300

    Add crossOrigin

--- src/main/java/br/unisinos/startup/connection/Application.java
+++ src/main/java/br/unisinos/startup/connection/Application.java
@@ -2,8 +2,10 @@ package br.unisinos.startup.connection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class Application {
 
 	public static void main(String[] args) {
